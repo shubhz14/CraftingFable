e5b4e0ed09dfd404fe0b707ade4ab02d68a3aa59
nagesh-badole <badole.nagesh@gmail.com>
2020-08-31 16:47:11 +0530

    horizontal scrollbar added

--- js/categories.js
+++ js/categories.js
@@ -1,20 +1,29 @@
 window.onload=function(){  
     const ele = document.getElementById('imageRow');
+    let pos = {left: 0, x: 0 };
     const mouseDownHandler = function(e) {
         ele.style.cursor = 'grabbing';
         ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            x: e.clientX,
+        };
 
         document.addEventListener('mousemove', mouseMoveHandler);
         document.addEventListener('mouseup', mouseUpHandler);
     };
 
     const mouseMoveHandler = function(e) {
-        ele.scrollLeft = e.clientX;
+        const dx = e.clientX - pos.x;
+        ele.scrollLeft = pos.left - dx;
     }
 
     const mouseUpHandler = function() {
         document.removeEventListener('mousemove', mouseMoveHandler);
         document.removeEventListener('mouseup', mouseUpHandler);
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
     };
 
     ele.addEventListener('mousedown', mouseDownHandler);
